5a89a96d34ef3a4a586ecc78132e12172c925f3e
Anderson Westfield <ajwestfield8889@gmail.com>
2025-01-19 16:06:04 -0800

    Initial Commit

---
+++ ODDS API/ODDS_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ODDS_APIApp.swift
+//  ODDS API
+//
+//  Created by Anderson Westfield  on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ODDS_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ODDS APITests/ODDS_APITests.swift
@@ -0,0 +1,17 @@
+//
+//  ODDS_APITests.swift
+//  ODDS APITests
+//
+//  Created by Anderson Westfield  on 1/19/25.
+//
+
+import Testing
+@testable import ODDS_API
+
+struct ODDS_APITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
